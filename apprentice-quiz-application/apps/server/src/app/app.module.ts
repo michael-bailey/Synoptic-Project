2273bc2fc4cad8835bc5c04a0878bfaa5344e0a8
michael-bailey <mickyb18a@gmail.com>
2022-01-26 10:53:56 +0000

    added database configuration

--- apprentice-quiz-application/apps/server/src/app/app.module.ts
+++ apprentice-quiz-application/apps/server/src/app/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const DB_HOST = process.env['DB_HOST'] ?? 'localhost';
+const DB_PORT = process.env['DB_PORT'] ?? '';
+
 @Module({
-  imports: [],
+  imports: [
+    // setting up database
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DB_HOST,
+      port: parseInt(DB_PORT),
+
+      username: 'main',
+      password: 'synoptic',
+
+      database: 'main',
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
