d68c7e6dbfffa70ae89d720eb0bcddbf315b87a4
michael-bailey <mickyb18a@gmail.com>
2022-01-27 20:15:07 +0000

    fixes for disabling server side rendering

--- apprentice-quiz-application/apps/client/pages/_app.tsx
+++ apprentice-quiz-application/apps/client/pages/_app.tsx
@@ -1,22 +1,50 @@
-import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import {
+  graphql,
+  loadQuery,
+  RelayEnvironmentProvider,
+  usePreloadedQuery,
+} from 'react-relay/hooks';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
+
 import RelayEnvironment from '../Relay/RelayEnvironment';
 
 // required for react bootstrap to work with next
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from 'next/link';
+import Topbar from '../Components/Navbars/Topbar';
+import { Suspense } from 'react';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
       <Head>
-        <title>Welcome to client!</title>
+        <title>Quiz App</title>
       </Head>
+
       <main className="app">
-        <Component {...pageProps} />
+        <SafeHydrate>
+          <Suspense fallback={'Loading'}>
+            <Topbar />
+            <Component {...pageProps} />
+          </Suspense>
+        </SafeHydrate>
       </main>
     </RelayEnvironmentProvider>
   );
 }
 
-export default CustomApp;
+// used to disable server side rendering,
+//as it doesn't currently work with suspense
+// which is used by react-relay
+function SafeHydrate({ children }) {
+  console.log(typeof window === 'undefined');
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  );
+}
+
+export default App;
