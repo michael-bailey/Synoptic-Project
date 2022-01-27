ba7fd8b5e164655adef15dcb541b5aa5f604337e
michael-bailey <mickyb18a@gmail.com>
2022-01-27 12:12:51 +0000

    added salt and hashing to password

--- apprentice-quiz-application/apps/server/src/app/auth/entities/Account.entity.ts
+++ apprentice-quiz-application/apps/server/src/app/auth/entities/Account.entity.ts
@@ -1,5 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import * as crypto from 'crypto';
+
 import { Session } from './Session.entity';
 
 @Entity()
@@ -16,11 +18,43 @@ export abstract class Account {
   @Column()
   _password: string;
 
-  @Column()
+  @Column({ default: '0' })
   _salt: string;
+
+  public set password(v: string) {
+    if (this._password == v) return;
+
+    console.log('[User] set password:', v);
+    this._salt = generateString(10);
+    this._password = crypto
+      .createHash('sha256')
+      .update(v + this._salt)
+      .digest('hex');
+    console.log('[User] set _password:', this._password);
+  }
+
+  public get password(): string {
+    return this._password;
+  }
 
   // todo: replace this with implementation
   checkPassword(password: string) {
-    return this._password == password;
+    const testPassword = crypto
+      .createHash('sha256')
+      .update(password + this._salt)
+      .digest('hex');
+    return this._password === testPassword;
   }
+}
+
+const characters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+export function generateString(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
