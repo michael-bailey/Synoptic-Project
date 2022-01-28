1b1002ae11e800e247d663c7742164ee1700d4c5
michael-bailey <mickyb18a@gmail.com>
2022-01-28 11:03:34 +0000

    added answers to the graphql schema

--- apprentice-quiz-application/apps/server/src/app/answer/answer.resolver.ts
+++ apprentice-quiz-application/apps/server/src/app/answer/answer.resolver.ts
@@ -1,7 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { Question } from '../question/entities/Question.entity';
 import { AnswerService } from './answer.service';
+import { Answer } from './entities/Answer.entity';
 
-@Resolver()
+@Resolver(() => Answer)
 export class AnswerResolver {
   constructor(private readonly answerService: AnswerService) {}
+
+  @ResolveField(() => Question)
+  async question(@Parent() parent: Answer) {
+    return await parent.question;
+  }
 }
--- apprentice-quiz-application/apps/server/src/app/question/question.resolver.ts
+++ apprentice-quiz-application/apps/server/src/app/question/question.resolver.ts
@@ -1,11 +1,29 @@
 import { NotFoundException } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { Answer } from '../answer/entities/Answer.entity';
+import { Quiz } from '../quiz/entities/Quiz.entity';
 import { Question } from './entities/Question.entity';
 import { QuestionService } from './question.service';
 
 @Resolver(() => Question)
 export class QuestionResolver {
   constructor(private readonly questionService: QuestionService) {}
+
+  @ResolveField(() => Quiz)
+  async quiz(@Parent() parent: Question) {
+    return await parent.quiz;
+  }
+
+  @ResolveField(() => [Answer])
+  async answers(@Parent() parent: Question) {
+    return await parent.answers;
+  }
 
   @Mutation(() => Question)
   async addQuestion(
